f559de1d7cd4fd97d2a40c5d5b3006dad38ae01f
Tony Vu <AnthonyVuOD@gmail.com>
2016-12-17 13:15:31 -0600

    updated portfolio

--- README.md
+++
@@ -1,2 +0,0 @@
-# portfolio
-My portfolio was built with HTML/CSS/Javascript/Angular 1/Responsive design/Photo editors.
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,88 +1,28 @@
-var app = angular
-  .module('portfolioModule',['ngRoute','ngAnimate'])
-  // will indicates what templated are used
-  .config(function($routeProvider){
-    $routeProvider.caseInsensitiveMatch= true;
-    $routeProvider
-      .when("/home",{
-        templateUrl:'templates/home.html',
-        controller:'homeCTRL'
-      })
-      .when("/about",{
-        templateUrl:'templates/about.html',
-        controller:'aboutCTRL'
-      })
-      .when("/portfolio",{
-        templateUrl:'templates/portfolio.html',
-        controller:'portfolioCTRL'
-      })
-      .when("/cv",{
-        templateUrl:'templates/cv.html',
-        controller:'cvCTRL'
-      })
-      .when("/contact",{
-        templateUrl:'templates/contact.html',
-        controller:'contactCTRL'
-      })
-      .otherwise({
-        redirectTo:"/home"
-      })
-  })
-  .controller('indexCTRL', function($rootScope, $scope, $location, $timeout){
-    $rootScope.$on('$routeChangeSuccess', function(){
-      $scope.landingPage = $location.path() === '/home';
-    });
-    // fade controls delay when images and headers appear
-    $scope.fade=false;
-    $timeout(function(){
-      $scope.fade=true;
-    },200);
-  })
-  .controller('homeCTRL', function($scope, $timeout, $location){
-    $scope.fade=false;
-    $timeout(function(){
-      $scope.fade=true;
-    },500);
+new Vue({
+  el: '#landingPage',
+  created: function () {
+    // parallax function when scrolling when window width > 1000px
+    window.addEventListener('scroll', function(){
+      if(window.innerWidth>1000){
+        var landingDiv = document.getElementById('landingDiv');
+        landingDiv.style.bottom = (window.pageYOffset *.11 + 25)+ "%";
+      } 
+    })
+    // reload when resizing window
+    window.onresize = function(){
+       location.reload();
+    }
+  }
+})
 
-    $scope.changePath = function(path) {
-      $location.path (path);
-    };
-  })
-  .controller('aboutCTRL', function($scope, $timeout){
-    $scope.fade=false;
-    $timeout(function(){
-      $scope.fade=true;
-    },400);
-  })
-  .controller('portfolioCTRL', function($scope, $timeout){
-    $scope.items = [
-      {project: "Greenbelt Climbing Routes", link:"https://anthonyvuod.github.io/GreenbeltClimbingRoutes/", delay: ".1s", photo:"photos/greenbeltSS.jpg"},
-      {project: "Travis Perkins Photography", link:"https://anthonyvuod.github.io/TravisPerkinsPhotography/", delay: ".2s", photo:"photos/travisPerkinsSS2.jpg"},
-      {project: "How Much House?", link:"https://anthonyvuod.github.io/HowMuchHouse/", delay: ".3s", photo:"photos/houseSS2.jpg"},
-      {project: "Vu Properties", link:"https://anthonyvuod.github.io/VuProperties/", delay: ".4s", photo:"photos/vuPropertiesSS.jpg"}
-    ];
-
-    $scope.fade=false;
-    $timeout(function(){
-      $scope.fade=true;
-    },400);
-  })
-  .controller('cvCTRL', function($scope, $timeout){
-    $scope.fade=false;
-    $timeout(function(){
-      $scope.fade=true;
-    },400);
-  })
-  .controller('contactCTRL', function($scope, $timeout){
-    $scope.contactItems = [
-      {link:'https://www.linkedin.com/in/anthony-vu-257a417?trk=nav_responsive_tab_profile', photo:'photos/linkedInIcon2.png'},
-      {link:'https://github.com/AnthonyVuOD', photo:'photos/gitHubIcon2.png'},
-      // {link:'https://www.facebook.com/profile.php?id=2007303', photo:'photos/facebookIcon.png'},
-      {link:'mailto:AnthonyVuOD@gmail.com', photo:'photos/gmailIcon2.png'}
-    ];
-
-    $scope.fade=false;
-    $timeout(function(){
-      $scope.fade=true;
-    },400);
-  });
+new Vue ({
+  el: '#portfolio',
+  data: {
+    projects: [
+      {name: "Greenbelt Climbing Routes", link:"https://anthonyvuod.github.io/GreenbeltClimbingRoutes/", photo:"images/greenbeltSS2.jpg"},
+      {name: "Travis Perkins Photography", link:"https://anthonyvuod.github.io/TravisPerkinsPhotography/", photo:"images/travisPerkinsSS2.jpg"},
+      {name: "How Much House?", link:"https://anthonyvuod.github.io/HowMuchHouse/", photo:"images/houseSS2.jpg"},
+      {name: "Vu Properties", link:"https://anthonyvuod.github.io/VuProperties/", photo: "images/vuPropertiesSS2.jpg"}
+    ]
+  }
+})
