a2c35ce030ac5384270197667215d502e42e51d2
Tony Vu <AnthonyVuOD@gmail.com>
2017-01-29 19:22:07 -0600

    API update

--- js/ODwebsite.js
+++ js/ODwebsite.js
@@ -1,6 +1,7 @@
+var map;
 function initMap() {
   var stPete = {lat: 27.773056, lng: -82.639999};
-  var map = new google.maps.Map(document.getElementById('map'),{
+  map = new google.maps.Map(document.getElementById('map'),{
     zoom: 10,
     center: stPete,
     scrollwheel:false
